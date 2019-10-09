6cfc442c34f1ea28799354a701f9374886675d53
Nate Stilwell <nathan.stilwell@move.com>
2019-10-09 15:59:11 -0400

    Create div for the bus contents

--- src/components/FixItBus.tsx
+++ src/components/FixItBus.tsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { blockStatement } from "@babel/types";
+import { NONAME } from "dns";
 
 export const FixItBus: React.FC = () => {
+  const busWrap = {
+    width: "200px",
+    margin: "20px",
+  };
+  const busHood = {
+    display: "block",
+    width: "100%",
+  };
+  const busDash = {
+    display: "block",
+    // padding: "3px",
+    width: "100%",
+  };
+  const busBody = {
+    display: "block",
+    padding: "3px",
+    borderTop: "none",
+    borderLeft: "4px solid #fdb303",
+    borderRight: "4px solid #fdb303",
+    borderBottom: "4px solid #fdb303",
+  };
   return (
-    <img
-      alt="the bus"
-      src="http://media.gettyimages.com/vectors/top-view-of-bright-yellow-realistic-school-bus-isolated-on-white-vector-id941821084?s=170667a&w=1007"
-    />
+    <div style={busWrap}>
+      <img alt="the bus" src="assets/bus.front.svg" style={busHood} />
+      <div style={busBody}>
+        <img alt="the bus" src="assets/bus.dash.svg" style={busDash} /> Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
+      </div>
+    </div>
   );
 };
