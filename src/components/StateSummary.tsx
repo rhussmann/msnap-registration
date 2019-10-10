1d3c178c7f5e3657128f2167cbf20f6acd9bec1a
Andrew Matheny <andrew.j.matheny@gmail.com>
2019-10-10 10:36:29 -0400

    Remove the duplicate spinner

--- src/components/StateSummary.tsx
+++ src/components/StateSummary.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useStateValue } from "../state/state";
 import { Typography } from "@material-ui/core";
-import { Spinner } from "./Spinner";
 
 export const StateSummary = () => {
   const { initialized, bus } = useStateValue()[0];
 
   if (initialized === false) {
-    return <Spinner />;
+    return null;
   }
 
   return <Typography variant="body1">{`${bus.seatsTaken} out of ${bus.capacity} seats taken`}</Typography>;
